fe0d40f94fb53f2c34e5dedfac31e34bd375dd56
StephenVarela <varelast92@gmail.com>
2018-09-11 13:49:58 -0400

    finished programming fundamentals reinforcement exercise

---
+++ programming_fundamentals.rb
@@ -0,0 +1,45 @@
+project = {
+  committee: ["Stella", "Salma", "Kai"],
+  title: "Very Important Project",
+  due_date: "December 14, 2019",
+  id: "3284",
+  steps: [
+    {description: "conduct interviews",
+     due_date: "August 1, 2018"
+    },
+    {description: "code of conduct",
+     due_date: "January 1, 2018"
+    },
+    {description: "compile results",
+     due_date: "November 10, 2018"
+    },
+    {description: "version 1",
+     due_date: "January 15, 2019"
+    },
+    {description: "revisions",
+     due_date: "March 30, 2019"
+    },
+    {description: "version 2",
+     due_date: "July 12, 2019"
+    },
+    {description: "final edit",
+     due_date: "October 1, 2019"
+    },
+    {description: "final version",
+     due_date: "November 20, 2019"
+    },
+    {description: "Wrap up",
+     due_date: "December 1, 2019"
+    }
+  ]
+}
+
+committee_member = 0;
+
+project[:steps].each do |step|
+  committee_member %= 3
+  step[:person] = project[:committee][committee_member]
+  committee_member += 1
+end
+
+puts project
